d3ddbb29e3d49930ae372c4d2b06ce70fcb464e8
fourst4r <oxygenmagnet@hotmail.com>
2020-03-22 17:18:27 +1300

    Update context.go

--- context.go
+++ context.go
@@ -1,21 +1,40 @@
 package router
 
+import "fmt"
+
 type Context struct {
 	Route   *Route
 	Client  Client
+	Channel Channel
 	Author  Author
 	Msg     Msg
 	Command string
 	Args    []string
 }
 
+// Say is a shortcut for Context.Client.Say()
+func (c *Context) Say(ch Channel, s string) {
+	c.Client.Say(ch, s)
+}
+
+func (c *Context) Reply(s string) {
+	c.Client.Say(c.Channel, fmt.Sprintf("%s %s", c.Author.Mention(), s))
+}
+
 type Client interface {
 	ID() string
+	Say(Channel, string)
+}
+
+type Channel interface {
+	ID() string
+	Name() string
 }
 
 type Author interface {
 	ID() string
 	Name() string
+	Mention() string
 }
 
 type Msg interface {
