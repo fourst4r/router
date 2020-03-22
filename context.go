44dfe8beb20bdd80a943387ca024cc80c1e4ea4e
fourst4r <oxygenmagnet@hotmail.com>
2020-03-23 12:18:10 +1300

    test a writer

--- context.go
+++ context.go
@@ -1,6 +1,9 @@
 package router
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 type Context struct {
 	Route   *Route
@@ -10,15 +13,36 @@ type Context struct {
 	Msg     Msg
 	Command string
 	Args    []string
+	Writer  io.StringWriter
 }
+
+// type Context interface {
+// 	Route() *Route
+// 	Client() Client
+// 	Channel() Channel
+// 	Author() Author
+// 	Msg() Msg
+// 	Command() string
+// 	Args() []string
+// }
 
 // Say is a shortcut for Context.Client.Say()
 func (c *Context) Say(ch Channel, s string) {
-	c.Client.Say(ch, s)
+	if c.Writer != nil {
+		c.Writer.WriteString(s)
+	} else {
+		c.Client.Say(ch, s)
+	}
 }
 
+// Reply sends s and mentions the user who ran the command.
 func (c *Context) Reply(s ...string) {
-	c.Client.Say(c.Channel, fmt.Sprintf("%s %s", c.Author.Mention(), fmt.Sprint(s)))
+	text := fmt.Sprintf("%s %s", c.Author.Mention(), fmt.Sprint(s))
+	if c.Writer != nil {
+		c.Writer.WriteString(text)
+	} else {
+		c.Client.Say(c.Channel, text)
+	}
 }
 
 type Client interface {
--- go.mod
+++ go.mod
@@ -3,9 +3,6 @@ module github.com/fourst4r/router
 go 1.13
 
 require (
-	github.com/Necroforger/dgrouter v0.0.0-20190528143456-040421b5a83e // indirect
-	github.com/andersfylling/disgord v0.16.5 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/bwmarrin/discordgo v0.20.2 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gempir/go-twitch-irc v1.1.0 // indirect
